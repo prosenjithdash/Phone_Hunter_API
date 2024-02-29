d6e4ac33dcf0a025cfa3faf2f98de698d0ac8046
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-29 18:08:43 +0600

    Setup done

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,36 @@
-console.log('hi')
+// JavaScript File
+
+// Load All Phone Data
+
+// Load Data Way number : 01
+
+// const loadPhoneDatas = () => {
+//     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+//         .then(res => res.json())
+//         .then(data => displayPhoneData(data))
+// } 
+
+// const displayPhoneData = (phones) => {
+//     console.log(phones)
+// }
+
+// loadPhoneDatas();
+
+
+
+
+
+
+
+// Load Data Way Number : 02 with async
+
+const loadPhones = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+
+    const data = await res.json()
+    const phones = data.data;
+    console.log(phones)
+
+    
+}
+loadPhones();
