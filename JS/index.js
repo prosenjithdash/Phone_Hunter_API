4ec321dc08f6a9ed526224424edc4049814f8c20
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-29 21:17:16 +0600

    Dynamically diplay data load and set done

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,11 @@
 
 // Load All Phone Data
 
+
+
+
+
+
 // Load Data Way number : 01
 
 // const loadPhoneDatas = () => {
@@ -29,8 +34,69 @@ const loadPhones = async () => {
 
     const data = await res.json()
     const phones = data.data;
-    console.log(phones)
+    // console.log(phones)
+    displayPhones(phones)
 
     
 }
+
+
+
+
+// Display Data
+
+const displayPhones = (phones) => {
+
+    // console.log(phones)
+
+    // single show phone
+
+
+
+
+    // for (const phone of phones) {
+    //     console.log(phone)
+    // }
+
+
+
+
+
+
+    // 1. get ui div with id 
+    const phonesDivContainer = document.getElementById('phone_container');
+
+
+    // single show phone with forEach
+
+    phones.forEach(phones => {
+        console.log(phones)
+
+        //2. create div
+
+        const phonescard = document.createElement('div');
+        phonescard.classList=`card w-96 bg-base-100 shadow-xl`
+
+        //3. set innerHTML
+
+        phonescard.innerHTML = `
+            <figure><img src=${phones.image} alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title justify-center">${phones.phone_name}</h2> 
+                        <p class='text-center'>${phones.slug}</p>
+                        <div class="card-actions justify-center">
+                            <button class="btn btn-primary">Show Details</button>
+                        </div>
+                    </div>
+        `
+
+        //4. set appendChild
+        
+        phonesDivContainer.appendChild(phonescard);
+
+    })
+ 
+   
+}
+
 loadPhones();
